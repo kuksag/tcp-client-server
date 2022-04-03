28eb5b02884d38732a4462c08bfa2d9db0e14913
kuksag <georgiy.kuksa@gmail.com>
2022-04-04 01:40:28 +0300

    Add threading to server

--- src/server.c
+++ src/server.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -9,22 +10,104 @@
 #include "common.h"
 
 int server_socket = -1;
-int peer_socket = -1;
 
 void cleanup() {
     if (server_socket != -1) {
         close(server_socket);
     }
-    if (peer_socket != -1) {
-        close(peer_socket);
+}
+
+int recv_wrapper(int peer_socket, void *data, size_t size) {
+    ssize_t len = recv(peer_socket, data, size, 0);
+    if (len == -1) {
+        perror("recv");
+        return 1;
+    } else if (len != size) {
+        fprintf(stderr,
+                "Didn't receive full package, expected: %zd, got: %zd\n", size,
+                len);
+        return 1;
     }
+    return 0;
+}
+
+int recv_file_wrapper(int peer_socket,
+                      int fd_out,
+                      size_t file_size,
+                      void *buffer,
+                      size_t buffer_size) {
+    size_t size = file_size;
+    ssize_t write_len;
+    ssize_t read_len;
+    do {
+        read_len = recv(peer_socket, buffer, sizeof(buffer_size), 0);
+        if (read_len == -1) {
+            perror("recv error");
+            return 1;
+        }
+        write_len = write(fd_out, buffer, read_len);
+        if (write_len == -1) {
+            perror("write error");
+            return 1;
+        }
+        size -= write_len;
+    } while (size > 0 && write_len > 0);
+    if (size != 0) {
+        fprintf(stderr,
+                "Didn't receive full package, expected: %zd, got: %zd\n",
+                file_size, file_size - size);
+        return 1;
+    }
+    return 0;
+}
+
+void *handler(void *data) {
+    int peer_socket = *(int *)data;
+    printf("Client accepted\n");
+
+    // ----- File size -----
+
+    off_t file_size;
+    if (recv_wrapper(peer_socket, &file_size, sizeof(file_size))) {
+        goto release_and_exit;
+    }
+
+    // ----- File name -----
+
+    char file_name[NAME_MAX];
+    if (recv_wrapper(peer_socket, file_name, sizeof(file_name))) {
+        goto release_and_exit;
+    }
+
+    // -----
+
+    printf("File size: %ld\n", file_size);
+    printf("Saving %s...\n", file_name);
+
+    // ----- File data -----
+    int fd_out = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd_out == -1) {
+        perror("open");
+        goto release_and_exit;
+    }
+    char buffer[BUFFER_SIZE];
+    if (recv_file_wrapper(peer_socket, fd_out, file_size, buffer,
+                          sizeof(buffer))) {
+        goto release_and_exit;
+    }
+
+    printf("%s saved\n", file_name);
+    // -----
+
+release_and_exit:
+    free(data);
+    close(peer_socket);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
     if (atexit(cleanup)) {
         PERROR_EXIT("setup() error")
-        perror("Error while setup");
-        exit(1);
     }
 
     server_socket = socket(DOMAIN, SOCK_STREAM, 0);
@@ -46,87 +129,20 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Listening...\n");
-
-    // client handler
-    // exit -- forbidden
     while (1) {
         struct sockaddr_in peer_addr;
 
+        pthread_t tid;
+        int peer_socket;
         socklen_t peer_addr_len = sizeof(peer_addr);
         if ((peer_socket = accept(server_socket, (struct sockaddr *)&peer_addr,
                                   &peer_addr_len)) == -1) {
             perror("Can't accept client");
             continue;
         }
-
-        printf("Client accepted\n");
-
-        ssize_t len;
-        // ----- File size -----
-
-        off_t file_size;
-        len = recv(peer_socket, &file_size, sizeof(file_size), 0);
-        if (len == -1) {
-            perror("recv error");
-            continue;
-        } else if (len != sizeof(file_size)) {
-            fprintf(stderr,
-                    "Didn't receive full package, expected: %zd, got: %zd\n",
-                    sizeof(file_size), len);
-            continue;
-        }
-
-        // ----- File name -----
-
-        char file_name[NAME_MAX];
-        len = recv(peer_socket, file_name, sizeof(file_name), 0);
-        if (len == -1) {
-            perror("recv error");
-            continue;
-        } else if (len != sizeof(file_name)) {
-            fprintf(stderr,
-                    "Didn't receive full package, expected: %zd, got: %zd\n",
-                    sizeof(file_name), len);
-            continue;
-        }
-
-        // ----- File data -----
-        int fd_out = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-        if (fd_out == -1) {
-            perror("open error");
-            continue;
-        }
-        size_t size = file_size;
-        ssize_t write_len;
-        ssize_t read_len;
-        char buffer[BUFFER_SIZE];
-        do {
-            read_len = recv(peer_socket, buffer, sizeof(buffer), 0);
-            if (read_len == -1) {
-                perror("recv error");
-                exit(EXIT_FAILURE);
-            }
-            write_len = write(fd_out, buffer, read_len);
-            if (write_len == -1) {
-                perror("write error");
-                exit(EXIT_FAILURE);
-            }
-            size -= write_len;
-        } while (size > 0 && write_len > 0);
-        if (size != 0) {
-            fprintf(stderr,
-                    "Didn't receive full package, expected: %zd, got: %zd\n",
-                    file_size, file_size - size);
-            continue;
-        }
-        // -----
-
-        printf("File size: %ld\n", file_size);
-        printf("File name: %s\n", file_name);
-
-        close(peer_socket);
+        int *arg = malloc(sizeof(peer_socket));
+        *arg = peer_socket;
+        pthread_create(&tid, NULL, handler, arg);
+        pthread_detach(tid);
     }
-    //
-
-    exit(EXIT_SUCCESS);
 }
